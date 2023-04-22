a42e46e006f53ec56205df4b3c9791c84a81cdd6
Jonathon Nolden <jnolden3927@gmail.com>
2023-04-22 00:01:16 -0700

    Added a few more components
    Added GenerateTeam, CreateMatch, MapTournament and SelectedHome components. Also a new folder for images.

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -18,8 +18,11 @@ function NavBar() {
 					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					<Navbar.Collapse id="responsive-navbar-nav">
 						<Nav className="me-auto">
-							<Nav.Link href="#features">Create Tournament</Nav.Link>
+							<Nav.Link href="/">Create Tournament</Nav.Link>
 							<Nav.Link href="#pricing">Players</Nav.Link>
+							<Nav.Link href="/selected">Selected</Nav.Link>
+							<Nav.Link href="/selected/createMatch">Create Match</Nav.Link>
+							<Nav.Link href="/selected/generate">Generate Match</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
